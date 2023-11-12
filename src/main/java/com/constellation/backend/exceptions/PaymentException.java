aa0a244fd5c1ea83ac7b0e5dcecb42d4741198bc
Ali Sheikhi <alish@my.yorku.ca>
2023-11-12 02:04:05 -0500

    Payment added

---
+++ src/main/java/com/constellation/backend/exceptions/PaymentException.java
@@ -0,0 +1,8 @@
+package com.constellation.backend.exceptions;
+
+public class PaymentException extends Exception {
+
+    public PaymentException(String message) {
+        super(message);
+    }
+}
