4c17b9230091d6254fa41ff5834eb285ca92685e
AlexM3 <k8.99e9@gmail.com>
2023-12-04 21:45:06 -0500

    highest bid flag

--- gateway/src/main/java/com/constellation/gateway/controllers/GatewayController.java
+++ gateway/src/main/java/com/constellation/gateway/controllers/GatewayController.java
@@ -32,7 +32,8 @@ public class GatewayController {
     @PostMapping(value = "/users/login", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> gateway(@RequestBody LoginRequest loginRequest) {
         try {
-            ResponseEntity<String> response = restTemplate.postForEntity(System.getenv("USERSERVICE_URL") + "/login", loginRequest, String.class);
+            ResponseEntity<String> response = restTemplate.postForEntity(
+                    System.getenv("USERSERVICE_URL") + "/login", loginRequest, String.class);
             return ResponseEntity.ok(response.getBody());
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Username or password is incorrect");
@@ -43,7 +44,8 @@ public class GatewayController {
     @PostMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> signup(@RequestBody SignupRequest signupRequest) {
         try {
-            ResponseEntity<String> response = restTemplate.postForEntity(System.getenv("USERSERVICE_URL") + "/", signupRequest, String.class);
+            ResponseEntity<String> response = restTemplate.postForEntity(
+                    System.getenv("USERSERVICE_URL") + "/", signupRequest, String.class);
             return ResponseEntity.ok(response.getBody());
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.CONFLICT, "User already exists with this username");
@@ -54,7 +56,9 @@ public class GatewayController {
     @PutMapping(value = "/users/{username}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> updateUser(@PathVariable String username, @RequestBody ChangePasswordRequest changePasswordRequest) {
         try {
-            ResponseEntity<String> response = restTemplate.exchange(System.getenv("USERSERVICE_URL") + "/" + username, HttpMethod.PUT, new HttpEntity<>(changePasswordRequest), String.class);
+            ResponseEntity<String> response = restTemplate.exchange(
+                    System.getenv("USERSERVICE_URL") + "/" + username, HttpMethod.PUT,
+                    new HttpEntity<>(changePasswordRequest), String.class);
             return ResponseEntity.ok(response.getBody());
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Couldn't find user");
@@ -85,7 +89,8 @@ public class GatewayController {
     public ResponseEntity<String> sellItem(@RequestBody SellItemRequest sellItemRequest) {
         System.out.println(sellItemRequest.getAuctionEnd());
         try {
-            ResponseEntity<String> response = restTemplate.postForEntity(System.getenv("CATALOGSERVICE_URL") + "/" , sellItemRequest, String.class);
+            ResponseEntity<String> response = restTemplate.postForEntity(
+                    System.getenv("CATALOGSERVICE_URL") + "/" , sellItemRequest, String.class);
             return ResponseEntity.ok(response.getBody());
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Couldn't sell item");
@@ -96,9 +101,10 @@ public class GatewayController {
         for (int i = 0; i < items.length(); i++) {
             JSONObject item = items.getJSONObject(i);
             try {
-                ResponseEntity<String> bidResponse = restTemplate.getForEntity(System.getenv("BIDSERVICE_URL") + "/highest/" + item.getInt("id"), String.class);
+                ResponseEntity<String> bidResponse = restTemplate.getForEntity(
+                        System.getenv("BIDSERVICE_URL") + "/highest/" + item.getInt("id"), String.class);
             } catch (Exception e) {
-                item.put("highestBid", item.get("initialPrice"));
+                item.put("highestBid", -1);
             }
         }
     }
